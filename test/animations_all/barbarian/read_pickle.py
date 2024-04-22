7d46beb490efd38c1243b34b1b2aee94d8608a56
jeromebyrne <jeromebyrne21@hotmail.com>
2024-04-21 22:24:05 -0700

    remove obj folder

---
+++ test/animations_all/barbarian/read_pickle.py
@@ -0,0 +1,22 @@
+import pickle
+import json
+import numpy as np
+
+def custom_converter(obj):
+    """Convert non-serializable objects for JSON."""
+    if isinstance(obj, np.ndarray):
+        return obj.tolist()  # Convert numpy arrays to list
+    # Add more conversions if necessary
+    raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
+
+def pickle_to_json(pickle_file_path, json_file_path):
+    # Load data from a pickle file
+    with open(pickle_file_path, 'rb') as file:
+        data = pickle.load(file)
+
+    # Convert the data to JSON format
+    with open(json_file_path, 'w') as file:
+        json.dump(data, file, default=custom_converter, indent=4)
+
+
+pickle_to_json('./attack_1713298069.7556002.pkl', 'unpickled.json')
