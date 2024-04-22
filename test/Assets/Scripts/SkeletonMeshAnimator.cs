76150064d50380475f6a5ae6c74437cf2ca3305a
jeromebyrne <jeromebyrne21@hotmail.com>
2024-04-22 15:21:22 -0700

    many updates  to bone rotations

--- test/Assets/Scripts/SkeletonMeshAnimator.cs
+++ test/Assets/Scripts/SkeletonMeshAnimator.cs
@@ -1,62 +1,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeshAnimator : MonoBehaviour
+public class SkeletonMeshAnimator : MonoBehaviour
 {
     private AnimDataImporter m_animDataImporter = new AnimDataImporter();
 
-    private int m_currentFrame = 0;
-    private float frameRate = 1/2.0f;
+    private int m_currentFrame = -1;
+    private float frameRate = 1/10.0f;
     private float lastFrameChange = 0.0f;
 
     [SerializeField]
     private GameObject joinVisualPrefab = null;
     private List<GameObject> jointVisuals = new List<GameObject>();
+
+    private int lockDisplayFrame = -1; // -1 to unlock and animate
+    private bool applyRotations = true;
+    private Vector2[] lastPositions;
     
     void Start()
     {
         m_animDataImporter.Import("unpickled.json");
 
-        bool isAnimatedMesh = true;
+        Mesh mesh = CreateMesh();
         
-        Mesh mesh = CreateMesh(isAnimatedMesh);
+        var bones = CreateSkeleton(m_animDataImporter.AnimData.joints_names, 
+                                            m_animDataImporter.AnimData.joints_parents,
+                                            m_animDataImporter.AnimData.joints,
+                                            m_animDataImporter.AnimData.a_pose_joints);
         
-        if (isAnimatedMesh)
-        {
-            var bones = CreateSkeleton(m_animDataImporter.AnimData.joints_names, 
-                                                m_animDataImporter.AnimData.joints_parents,
-                                                m_animDataImporter.AnimData.joints,
-                                                m_animDataImporter.AnimData.a_pose_joints);
-
-            SetupBoneWeightsAndPoses(mesh, bones, gameObject.GetComponent<SkinnedMeshRenderer>());
-        }
+        SkinnedMeshRenderer skinnedMeshRenderer = gameObject.GetComponent<SkinnedMeshRenderer>();
+        SetupBoneWeightsAndPoses(mesh, bones, skinnedMeshRenderer);
     }
     
-    Mesh CreateMesh(bool isSkinnedMesh)
+    Mesh CreateMesh()
     {
         Mesh mesh = new Mesh();
-
-        if (!isSkinnedMesh)
-        {
-            MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
-            meshFilter.mesh = mesh;
         
-            MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
-            if (renderer == null) {
-                renderer = gameObject.AddComponent<MeshRenderer>();
-            }
-            renderer.material = Resources.Load<Material>("barbarian_mat");
+        SkinnedMeshRenderer skinnedRenderer = gameObject.GetComponent<SkinnedMeshRenderer>();
+        if (skinnedRenderer == null) {
+            skinnedRenderer = gameObject.AddComponent<SkinnedMeshRenderer>();
         }
-        else
-        {
-            SkinnedMeshRenderer skinnedRenderer = gameObject.GetComponent<SkinnedMeshRenderer>();
-            if (skinnedRenderer == null) {
-                skinnedRenderer = gameObject.AddComponent<SkinnedMeshRenderer>();
-            }
-            skinnedRenderer.material = Resources.Load<Material>("barbarian_mat");
+        skinnedRenderer.material = Resources.Load<Material>("barbarian_mat");
 
-            skinnedRenderer.sharedMesh = mesh;
-        }
+        skinnedRenderer.sharedMesh = mesh;
         
         var vertData = m_animDataImporter.AnimData.vertices_uv;
     
@@ -109,19 +95,21 @@ public class MeshAnimator : MonoBehaviour
                                 List <float[] > a_poseJoints)
     {
         Transform[] bones = new Transform[jointNames.Length];
-
+        
+        lastPositions = new Vector2[jointNames.Length];
+        
         for (int i = 0; i < jointNames.Length; i++) 
         {
-            GameObject bone = new GameObject(jointNames[i]);  // Create a bone as GameObject
-            
-            // set initial pose
-            bone.transform.position = new Vector3(a_poseJoints[i][0], a_poseJoints[i][1], 0.0f); // Adjust accordingly
-            
-            bones[i] = bone.transform;  // Store the Transform component
+            GameObject bone = new GameObject(jointNames[i]); 
+            bone.transform.localPosition = new Vector3(a_poseJoints[i][0], a_poseJoints[i][1], 0.0f);  // Local position set
+
+            bones[i] = bone.transform;  
             if (jointParents[i] != -1) 
-            {  // Assuming -1 indicates no parent
-                bones[i].parent = bones[jointParents[i]];  // Set parent using Transform components
+            {  
+                bones[i].parent = bones[jointParents[i]]; 
             }
+            // Store initial local position
+            lastPositions[i] = bones[i].localPosition; 
 
             // create debug joint image
             GameObject jointVisual = Instantiate(joinVisualPrefab, bone.transform.position, Quaternion.identity);
@@ -129,6 +117,8 @@ public class MeshAnimator : MonoBehaviour
             DebugJointVisualizer djv = jointVisual.GetComponent<DebugJointVisualizer>();
             djv.m_SpriteRenderer.color = Color.green;
             djv.m_text.text = jointNames[i];
+            
+            jointVisuals.Add(jointVisual);
         }
 
         return bones;
@@ -153,39 +143,6 @@ public class MeshAnimator : MonoBehaviour
         BoneWeightCalculator bwCalc = gameObject.AddComponent<BoneWeightCalculator>();
         bwCalc.AssignBoneWeights(renderer);
     }
-    
-    BoneWeight CreateRandomBoneWeight(int numberOfBones)
-    {
-        int numberOfInfluences = 1;
-        
-        BoneWeight weight = new BoneWeight();
-        
-        // Generate random indices and weights
-        int[] indices = new int[numberOfInfluences];
-        float[] weights = new float[numberOfInfluences];
-        float totalWeight = 0;
-
-        for (int i = 0; i < numberOfInfluences; i++)
-        {
-            indices[i] = Random.Range(0, numberOfBones);
-            weights[i] = Random.value;
-            totalWeight += weights[i];
-        }
-
-        // Normalize weights
-        for (int i = 0; i < numberOfInfluences; i++)
-        {
-            weights[i] /= totalWeight;
-        }
-
-        // Assign normalized weights and indices
-        weight.boneIndex0 = indices[0]; weight.weight0 = weights[0];
-        if (numberOfInfluences > 1) { weight.boneIndex1 = indices[1]; weight.weight1 = weights[1]; }
-        if (numberOfInfluences > 2) { weight.boneIndex2 = indices[2]; weight.weight2 = weights[2]; }
-        if (numberOfInfluences > 3) { weight.boneIndex3 = indices[3]; weight.weight3 = weights[3]; }
-
-        return weight;
-    }
 
     void Animate()
     {
@@ -193,57 +150,119 @@ public class MeshAnimator : MonoBehaviour
 
         int numFrames = m_animDataImporter.AnimData.joints.Count;
 
+        int nextFrame = m_currentFrame;
         if (lastFrameChange <= 0.0f)
         {
-            m_currentFrame++;
-            if (m_currentFrame == numFrames)
+            nextFrame++;
+            if (nextFrame >= numFrames)
             {
-                m_currentFrame = 0;
+                nextFrame = 0;
             }
             
             lastFrameChange = frameRate;
         }
 
-        var frameJoints = m_animDataImporter.AnimData.joints[m_currentFrame];
+        SetDisplayFrame(nextFrame);
+    }
 
-        SkinnedMeshRenderer renderer = gameObject.GetComponent<SkinnedMeshRenderer>();
-        if (frameJoints.Count != renderer.bones.Length)
+    void SetDisplayFrame(int frame)
+    {
+        if (m_currentFrame == frame)
         {
             return;
         }
 
+        m_currentFrame = frame;
+        
+        var frameJoints = m_animDataImporter.AnimData.joints[m_currentFrame];
+
+        SkinnedMeshRenderer renderer = gameObject.GetComponent<SkinnedMeshRenderer>();
+        
         for (int i = 0; i < renderer.bones.Length && i < frameJoints.Count; i++)
         {
-            renderer.bones[i].position = new Vector3(frameJoints[i][0], frameJoints[i][1], 0);
-            // renderer.bones[i].localScale = new Vector3(5.0f, 5.0f, 1.0f);
+            Vector3 newPosition = new Vector3(frameJoints[i][0], frameJoints[i][1], 0);
+            renderer.bones[i].position = newPosition;
+        }
+    }
+
+    void ApplyJointRotations()
+    {
+        if (!applyRotations)
+        {
+            return;
+        }
+    
+        var frameJoints = m_animDataImporter.AnimData.joints[m_currentFrame];
+        SkinnedMeshRenderer renderer = gameObject.GetComponent<SkinnedMeshRenderer>();
+
+        // Process each bone according to the hierarchy defined by joints_parents
+        for (int i = 0; i < renderer.bones.Length; i++)
+        {
+            if (renderer.bones[i] == null || i >= frameJoints.Count)
+                continue;
+
+            int parentIndex = m_animDataImporter.AnimData.joints_parents[i];
+            Transform parentBone = (parentIndex != -1 && parentIndex < renderer.bones.Length) ? renderer.bones[parentIndex] : null;
+
+            if (parentBone != null)
+            {
+                ApplyRotation(renderer.bones[i], parentBone, i, parentIndex);
+            }
+        }
+    }
+    
+    void ApplyRotation(Transform bone, Transform parentBone, int index, int parentIndex)
+    {
+        Vector2 currentParentPos = new Vector2(parentBone.localPosition.x, parentBone.localPosition.y);
+        Vector2 currentChildPos = new Vector2(bone.localPosition.x, bone.localPosition.y);
+
+        Vector2 lastParentPos = lastPositions[parentIndex];
+        Vector2 lastChildPos = lastPositions[index];
+
+        Vector2 previousDirection = lastChildPos - lastParentPos;
+        Vector2 currentDirection = currentChildPos - currentParentPos;
+
+        if (previousDirection == Vector2.zero || currentDirection == Vector2.zero)
+        {
+            return; // Avoid zero vector normalization
+        }
+
+        previousDirection.Normalize();
+        currentDirection.Normalize();
+
+        float angle = Vector2.SignedAngle(previousDirection, currentDirection);
+        bone.localRotation = Quaternion.Euler(0, 0, bone.localRotation.eulerAngles.z + angle);
+
+        // Update last known positions
+        lastPositions[index] = currentChildPos;
+        lastPositions[parentIndex] = currentParentPos;
+    }
+
+    void UpdateDebugJoints()
+    {
+        SkinnedMeshRenderer renderer = gameObject.GetComponent<SkinnedMeshRenderer>();
+        
+        for (int i = 0; i < renderer.bones.Length; i++)
+        {
+            jointVisuals[i].transform.position = renderer.bones[i].position;
         }
     }
     
     void Update()
     {
-        Animate();
-
-        DrawTriangles();
-
-        // LogBoneWeights(GetComponent<SkinnedMeshRenderer>());
-    }
-    
-    void OnDrawGizmos()
-    {
-        /*
-        SkinnedMeshRenderer renderer = gameObject.GetComponent<SkinnedMeshRenderer>();
-        if (renderer == null)
+        
+        if (lockDisplayFrame > -1)
         {
-            return;
+            SetDisplayFrame(lockDisplayFrame);
+        }
+        else
+        {
+            Animate();
         }
         
-        Gizmos.color = Color.red;
+        ApplyJointRotations();
 
-        foreach (Transform bone in renderer.bones)
-        {
-            Gizmos.DrawSphere(bone.position, 0.01f);
-        }
-        */
+        UpdateDebugJoints();
     }
 
     void DrawTriangles()
@@ -276,17 +295,6 @@ public class MeshAnimator : MonoBehaviour
             Debug.DrawLine(p1, p2, lineColor, duration);
             Debug.DrawLine(p2, p3, lineColor, duration);
             Debug.DrawLine(p3, p1, lineColor, duration);
-        }
-    }
-    
-    void LogBoneWeights(SkinnedMeshRenderer renderer)
-    {
-        Mesh mesh = renderer.sharedMesh;
-        BoneWeight[] weights = mesh.boneWeights;
-
-        for (int i = 0; i < weights.Length; i++)
-        {
-            Debug.Log("Vertex " + i + " weights: " + weights[i].weight0 + ", " + weights[i].weight1);
         }
     }
 }
